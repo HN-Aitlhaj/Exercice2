a9d4c02df4e462c3285c4617cbfbe3765e1a9764
Hanane Aït-Lhaj <92375851+HN-Aitlhaj@users.noreply.github.com>
2021-10-16 20:34:19 +0100

    trier

--- Program.cs
+++ Program.cs
@@ -9,6 +9,37 @@ namespace Exercice2
             Console.WriteLine("Veuillez saisir la taille du tableau");
             string number = Console.ReadLine();
             int taille = Convert.ToInt32(number);
+
+            int[] Tab = new int[taille];
+
+            Console.WriteLine("Veuillez saisir les elements du tableau :");
+
+            for (int i = 0; i < taille; i++)
+            {
+                Console.Write("l'element N° {0} :  ",i+1);
+                Tab[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(Tab);
+
+            Console.WriteLine("triage du tableau");
+            int j;
+            for (int i = 0; i < taille; i++)
+            {
+                Console.Write(" "+Tab[i]+" ");
+                j = i;
+            }
+            Array.Resize(ref Tab, Tab.Length + 1);
+
+            Console.Write("\n\n Veuillez saisir la valeur à insérer: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Tab[j] = num;
+
+            for (int i = 0; i < Tab.Length; i++)
+            {
+                Array.Sort(Tab);
+                Console.Write("\t {1} |", (i + 1), Tab[i]);
+
+            }
         }
     }
 }
