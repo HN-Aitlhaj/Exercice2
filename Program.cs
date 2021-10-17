66654e814b051d071a4efb1eab9a66991ecada4e
Hanane Aït-Lhaj <92375851+HN-Aitlhaj@users.noreply.github.com>
2021-10-17 13:40:41 +0100

    Last update

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ namespace Exercice2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Veuillez saisir la taille du tableau");
+            Console.Write(" Veuillez saisir la taille du tableau :\t");
             string number = Console.ReadLine();
             int taille = Convert.ToInt32(number);
 
             int[] Tab = new int[taille];
 
-            Console.WriteLine("Veuillez saisir les elements du tableau :");
+            Console.WriteLine("\n Veuillez saisir les elements du tableau :");
 
             for (int i = 0; i < taille; i++)
             {
@@ -20,26 +20,38 @@ namespace Exercice2
                 Tab[i] = Convert.ToInt32(Console.ReadLine());
             }
 
-            Console.WriteLine("triage du tableau");
+            //triage du tableau :
+            Console.WriteLine("\n triage du tableau :");
             Array.Sort(Tab);
             int j = 0;
             for (int i = 0; i < taille; i++)
             {
-                Console.Write("\t"+Tab[i]+" ");
+                Console.Write("\t"+Tab[i]+" |");
                 j = i;
             }
-            Array.Resize(ref Tab, Tab.Length + 1);
 
-            Console.Write("\n Veuillez saisir la valeur à insérer: ");
+            //changer la taille du tableau :
+            Array.Resize(ref Tab, taille + 1);
+            //Array.Resize(ref Tab, Tab.length + 1);
+
+            //l'insertion de la nouvelle valeur :
+            Console.WriteLine("\n Veuillez saisir la valeur à insérer: ");
             int num = Convert.ToInt32(Console.ReadLine());
-            Tab[j] = num;
+            Tab[j+1] = num;
 
-            for (int i = 0; i < Tab.Length; i++)
+            //triage du tableau apres l'insertion de la nouvelle valeur :
+            Console.WriteLine("\n triage du tableau apres l'insertion :");
+
+            Array.Sort(Tab);
+            taille = Tab.Length;
+
+            for (int i = 0; i < taille; i++)
             {
-                Array.Sort(Tab);
-                Console.Write("\Tab {1} |", (i + 1), Tab[i]);
+                Console.Write("\t {0} |", Tab[i]);
 
             }
         }
     }
 }
+
+//
